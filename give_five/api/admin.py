55fa5ba5166afc65b46675cb7b15220bfda26c34
Vitalino13 <vitaliiast93@gmail.com>
2023-01-23 14:30:21 +0200

    Created  models.py

--- give_five/api/admin.py
+++ give_five/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Theme
+
+class ThemeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.register(Theme, ThemeAdmin)
--- give_five/api/models.py
+++ give_five/api/models.py
@@ -1,4 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+QUESTION_CHOISES = (
+    ('single', 'Единственный правильный ответ'),
+    ('multiple', 'Несколько правильных ответов'),
+    ('insertion', 'Ввод ответов через пробел'),
+    ('correspond', 'Соотнесение вопросов и ответов'),
+)
+
+GRADE_CHOISES = (
+    ('junior', '1-4 класс'),
+    ('five', '5 класс'),
+    ('six', '6 класс'),
+    ('seven', '7 класс'),
+    ('eight', '8 класс'),
+    ('nine', '9 класс'),
+    ('oge', 'ОГЭ'),
+    ('ege', 'ЕГЭ'),
+)
+
+user = get_user_model()
 
 
 class Documentation(models.Model):
@@ -9,7 +30,7 @@ class Documentation(models.Model):
         verbose_name='ключ',
         help_text='Введите ключ, который будет использоваться на сайте.',
         max_length=256,
-        )
+    )
     description = models.TextField(
         verbose_name='значение',
         help_text='Введите текст, который будет отображаться на сайте.',
@@ -22,21 +43,16 @@ class Documentation(models.Model):
 
 
 class Question(models.Model):
-    title = models.CharField(
+    title = models.TextField(
         verbose_name='Текст вопроса. Если это вопрос на соотношение,'
                      'то в виде {"А": "текст вопроса 1", "Б": "текст вопроса 2"}',
         help_text='Введите текст вопроса',
+    )
+    multiple = models.CharField(
+        verbose_name='Тип вопроса',
+        help_text='Выберите тип вопроса',
         max_length=256,
-    )
-    multiple = models.BooleanField(
-        verbose_name='Множественный выбор',
-        help_text='Выберите, если у вопроса несколько правильных ответов',
-        default=False,
-    )
-    correspond = models.BooleanField(
-        verbose_name='Соотнешение',
-        help_text='Выберите, если это вопрос на соотношение вопрос-ответ',
-        default=False,
+        choices=QUESTION_CHOISES
     )
     theme = models.ForeignKey(
         'Theme',
@@ -49,15 +65,14 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
-    title = models.CharField(
+    title = models.TextField(
         verbose_name='Ответ',
         help_text='Введите текст ответа. Если это вопрос на соотношение,'
                   ' то в виде {"1": "текст ответа 1", "2": "текст ответа 2"}',
-        max_length=256,
     )
     is_right = models.BooleanField(
         verbose_name='Правильность',
-        help_text='Правильный ли ответ',
+        help_text='Отметьте, если ответ правильный',
         default=False,
     )
     question = models.ForeignKey(
@@ -74,3 +89,75 @@ class Answer(models.Model):
     class Meta:
         verbose_name = 'Ответ'
         verbose_name_plural = 'Ответы'
+
+
+class Theme(models.Model):
+    title = models.CharField(
+        verbose_name='Заголовок теста',
+        help_text='Введите заголовок теста ',
+        max_length=256,
+    )
+    description = models.TextField(
+        verbose_name='Описание теста',
+        help_text='Введите описания для данного теста',
+    )
+    theme_slug = models.SlugField(
+        max_length=256,
+        unique=True,
+    )
+    grade = models.ForeignKey(
+        'Grade',
+        on_delete=models.CASCADE,
+        related_name='theme_test'
+    )
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+
+
+class Grade(models.Model):
+    level = models.CharField(
+        verbose_name='Класс или Экзамен',
+        help_text='Введите номер класса или аббревиатуру экзамены',
+        choices=GRADE_CHOISES,
+    )
+
+
+class Result(models.Model):
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Количество баллов'
+    )
+    finished_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата прохождения теста',
+    )
+    offer_course = models.BooleanField(
+        verbose_name='Предложение курса',
+        help_text='Предлагать курс только при плохом прохождении теста',
+        default=False,
+    )
+    user = models.ForeignKey(
+        user,
+        on_delete=models.CASCADE,
+    )
+    theme = models.ForeignKey(
+        'Theme',
+        on_delete=models.CASCADE,
+    )
+    monetization = models.ForeignKey(
+        'Monetization',
+        on_delete=models.PROTECT
+    )
+
+class Meta:
+    # Если мы храним только последний пройденный тест для данного пользователя по данной теме
+    constraints = [
+        models.UniqueConstraint(
+            fields=['user', 'theme'],
+            name='user_passed_test'
+        )
+    ]
+    verbose_name = 'Результат'
+    verbose_name_plural = 'Результаты'
+    ordering = ('finished_at',)
